e98b9b73da062e9d43bb59f135325d0b7554eaed
Aoi1011 <ht.so.ik0102@gmail.com>
2022-09-18 09:59:58 +0700

    Create a new file for Header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,13 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
     <div>
-       
+      <h1 className="text-3xl font-bold underline">Hello world</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

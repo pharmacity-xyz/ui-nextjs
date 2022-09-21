015a7d3b6a41e5ed9de4800601f222128b637768
Aoi1011 <ht.so.ik0102@gmail.com>
2022-09-21 21:13:38 +0700

    Update home and about page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,56 +4,9 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Slider from '../components/Slider'
 
-import img1 from '../public/images/hero_bg_2.jpg'
-
-const introductions = [
-  {
-    id: 1,
-    title: 'Free Delivery',
-    detail:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa laborum voluptates excepturi neque labore .',
-    loadMore: '/free',
-  },
-  {
-    id: 2,
-    title: 'New Medicine Everyday',
-    detail:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa laborum voluptates excepturi neque labore .',
-    loadMore: '/new-medicine-everyday',
-  },
-  {
-    id: 3,
-    title: 'Medicines Guaranteed',
-    detail:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa laborum voluptates excepturi neque labore .',
-    loadMore: '/medicines-guranteed',
-  },
-]
-
 const Home: NextPage = () => {
   return (
     <div>
-      <div className="w-full h-1/4">
-        <Image src={img1} width={500} height={200} layout="responsive" />
-      </div>
-      <div className="py-5">
-        <div className="">
-          <div className="grid grid-cols-3 gap-3">
-            {introductions.map((introduction) => (
-              <div
-                key={introduction.id}
-                className="w-full text-center border rounded-md p-3"
-              >
-                <span className=""></span>
-                <h3 className="text-[#75b239] font-medium text-xl">
-                  {introduction.title}
-                </h3>
-                <p>{introduction.detail}</p>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
       <div className="bg-[#f8f9fa]">
         <div className="my-10">
           <div className="flex justify-between items-center px-8">

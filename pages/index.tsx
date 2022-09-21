3b6d2560a60d8008f22642d022986c45b56e4249
Aoi1011 <ht.so.ik0102@gmail.com>
2022-09-21 21:30:19 +0700

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,28 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Slider from '../components/Slider'
 
+import scheduleVaccineImg from '../public/images/schedule_vaccine.png'
+
 const Home: NextPage = () => {
   return (
     <div>
+      <div className="flex items-center bg-[#F5f5f0] py-8 px-10">
+        <h1 className="text-4xl w-1/3">About COVID-19</h1>
+        <div className="flex justify-around w-2/3">
+          <div className="text-center">
+            <Image src={scheduleVaccineImg} width="100" height="100" />
+            <h5>Vaccine</h5>
+          </div>
+          <div className="text-center">
+            <Image src={scheduleVaccineImg} width="100" height="100" />
+            <h5>Flu shots are here</h5>
+          </div>
+          <div className="text-center">
+            <Image src={scheduleVaccineImg} width="100" height="100" />
+            <h5>COVID-19 testing options</h5>
+          </div>
+        </div>
+      </div>
       <div className="bg-[#f8f9fa]">
         <div className="my-10">
           <div className="flex justify-between items-center px-8">

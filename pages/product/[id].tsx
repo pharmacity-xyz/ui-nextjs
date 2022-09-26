0abffc29242a707149b63628f28e8cde07a3de11
Aoi1011 <ht.so.ik0102@gmail.com>
2022-09-26 22:36:16 +0700

    Add table in product page

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -55,15 +55,62 @@ const ProductDetail: React.FC<Props> = ({ product }) => {
         </div>
       </div>
       <div>
-        <h1>Product Information</h1>
-        <div>
-          <table>
-            <tbody>
-              <tr>
-                <td>Brand</td>
-              </tr>
-            </tbody>
-          </table>
+        <div className="text-center my-4">
+          <h1 className="text-2xl">Product Detail</h1>
+        </div>
+        <div className="flex flex-col p-4">
+          <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+              <div className="overflow-hidden">
+                <table className="min-w-full">
+                  <thead className="border-b bg-slate-700 text-center">
+                    <tr>
+                      <th
+                        scope="col"
+                        className="text-sm font-medium text-white px-6 py-4"
+                      >
+                        Product Id
+                      </th>
+                      <th
+                        scope="col"
+                        className="text-sm font-medium text-white px-6 py-4"
+                      >
+                        Product Name
+                      </th>
+                      <th
+                        scope="col"
+                        className="text-sm font-medium text-white px-6 py-4"
+                      >
+                        Category
+                      </th>
+                      <th
+                        scope="col"
+                        className="text-sm font-medium text-white px-6 py-4"
+                      >
+                        Price
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="">
+                    <tr className="border-b">
+                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                        1
+                      </td>
+                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        Mark
+                      </td>
+                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        Otto
+                      </td>
+                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        @mdo
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>

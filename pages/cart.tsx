f91a8a01b6fdbda90a792c2da77d34c4e6dd0fcc
Aoi1011 <ht.so.ik0102@gmail.com>
2022-09-26 23:23:53 +0700

    Update key

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -11,7 +11,7 @@ const Cart = () => {
         <p className="text-right">Price</p>
         <hr />
         {carts.map((cart) => (
-          <div>
+          <div key={cart.id}>
             <Image src={cart.image} alt={cart.name} width={100} height={100} />
           </div>
         ))}

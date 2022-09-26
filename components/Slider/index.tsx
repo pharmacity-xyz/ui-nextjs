98b1b82de639b80b2a3be144782bd2c51184faf7
Aoi1011 <ht.so.ik0102@gmail.com>
2022-09-26 17:49:00 +0700

    Update

--- components/Slider/index.tsx
+++ components/Slider/index.tsx
@@ -51,7 +51,10 @@ const Slider = (props) => {
   return (
     <Carousel ssr responsive={responsive} className="text-center z-10">
       {featuredProducts.map((product) => (
-        <Link href={{ pathname: '/product/[id]', query: { id: product.id } }}>
+        <Link
+          href={{ pathname: '/product/[id]', query: { id: product.id } }}
+          key={product.id}
+        >
           <div className="container m-1 cursor-pointer hover:scale-105 transform">
             <Image src={product.image} width={100} height={200} />
             <h2 className="text-lg">{product.name}</h2>

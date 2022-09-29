449bb5106c933063b5780af7970fd69b9695a842
Aoi1011 <ht.so.ik0102@gmail.com>
2022-09-29 21:07:01 +0700

    fix the issue

--- components/Slider/Medical.tsx
+++ components/Slider/Medical.tsx
@@ -11,12 +11,12 @@ const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
     items: 1,
-    slidesToSlide: 3,
+    slidesToSlide: 1,
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
     items: 1,
-    slidesToSlide: 2,
+    slidesToSlide: 1,
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },

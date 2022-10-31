d06798ad05d0a4a1b6406f6e96bf4fbb951528b5
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-31 20:18:43 +0700

    Update

--- apis/axios.ts
+++ apis/axios.ts
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_ENV_VARIABLE,
-  timeout: 3000,
+  baseURL: "http://localhost:8000",
 })
 
 instance.interceptors.response.use(
---
+++ services/auth/types.ts
@@ -0,0 +1,14 @@
+export interface ISignUpApiData {
+  email: string
+  password: string
+  first_name: string
+  last_name: string
+  city: string
+  country: string
+  company_name: string
+}
+
+export interface ILogInApiData {
+  email: string
+  password: string
+}
